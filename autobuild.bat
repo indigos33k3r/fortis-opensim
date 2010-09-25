e3bf558fb1b7276a5c5566b50b62dd404da2f86c
Latif Khalifa <latifer@radegastclient.org>
2010-09-25 05:07:55 +0200

    Smarter batch param parsing

--- autobuild.bat
+++ autobuild.bat
@@ -1,11 +1,25 @@
 @echo off
+
+set makearch=
+set makedist=
+
+rem Set flags according to passed command line params
+:ParamLoop
+IF "%1"=="" GOTO ParamContinue
+IF "%1"=="arch" set makearch=yes
+IF "%1"=="dist" set makedist=yes
+SHIFT
+GOTO ParamLoop
+:ParamContinue
+
 rem use .NET 3.5 to build
 Prebuild.exe /target vs2008
-IF  NOT ERRORLEVEL 0 GOTO FAIL
+IF NOT ERRORLEVEL 0 GOTO FAIL
 
 %WINDIR%\Microsoft.NET\Framework\v3.5\msbuild opensim.sln
-IF  NOT ERRORLEVEL 0 GOTO FAIL
+IF NOT ERRORLEVEL 0 GOTO FAIL
 
+IF NOT "%makearch%"=="yes" GOTO SkipArch
 echo Build success, creating zip package
 cd bin
 rmdir /q /s fortis-opensim-autobuild
@@ -13,7 +27,7 @@ ren Debug fortis-opensim-autobuild
 del /q fortis-opensim-autobuild.zip
 7z -tzip a fortis-opensim-autobuild.zip fortis-opensim-autobuild
 cd ..
-
+:SkipArch
 
 :SUCCESS
 exit /B 0
